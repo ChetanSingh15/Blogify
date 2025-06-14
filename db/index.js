cb27da783d0fe16f2451703317de1d1253524ff0
ChetanSingh15 <chetanrabary@gmail.com>
2025-06-14 07:55:37 +0530

    Stop tracking .env file

--- db/index.js
+++ db/index.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const DB_NAME = "Blogify";
 
-// const MONGO_URI = "mongodb+srv://chetan:<Enter your password here>@cluster0.xc4e4.mongodb.net"
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${MONGO_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB Connected!! DB host: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDB Connection Failed",error)
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
 import { app } from "./app.js"
 
-const PORT = process.env.PORT || 8000;
+dotenv.config({
+    path: './.env'
+})
 
 
 connectDB()
@@ -9,8 +12,8 @@ connectDB()
     app.on("error",(error) => {
         console.log("Error: ",error)
     })
-    app.listen(PORT,() => {
-        console.log(`Server is running on port: ${PORT}`)
+    app.listen(process.env.PORT||8000,() => {
+        console.log(`Server is running on port: ${process.env.PORT||8000}`)
     })
 })
 .catch((err) => {
