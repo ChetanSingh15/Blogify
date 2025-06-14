05e3c5e306a765b3a3738500da25a07ff014f14e
ChetanSingh15 <chetanrabary@gmail.com>
2025-06-14 08:12:53 +0530

    mongo uri

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const DB_NAME = "Blogify";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
         console.log(`\n MongoDB Connected!! DB host: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDB Connection Failed",error)
